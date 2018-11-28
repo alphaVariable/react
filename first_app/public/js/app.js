8fb43b51bc3130c2209bae3741db67cda8aa936d
alphaVariable <vikasoffice81@gmail.com>
2018-11-28 23:40:48 +0800

    fixes the error: SyntaxError: expected expression, got '<'

--- first_app/public/js/app.js
+++ first_app/public/js/app.js
@@ -1,15 +1,14 @@
 class ProductList extends React.Component {
-    render() {
-      return (
-        <div className='ui unstackable items'>
-          Hello, friend! I am a basic React component.
+  render() {
+    return (
+      <div className='ui unstackable items'>
+        Hello, friend! I am a basic React component.
         </div>
-      );
-    }
+    );
   }
-  
-  ReactDOM.render(
-    <ProductList />,
-    document.getElementById('content')
-  );
-  
+}
+
+ReactDOM.render(
+  <ProductList />,
+  document.getElementById('content')
+);
