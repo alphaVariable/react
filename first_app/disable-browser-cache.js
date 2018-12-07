b1f7bfcf1615d722e0c5e4d68eac7ff83fad3628
Vikas Thakur <vikas@staff.iinet.net.au>
2018-12-07 15:29:35 +0800

    adjust spacing

--- first_app/disable-browser-cache.js
+++ first_app/disable-browser-cache.js
@@ -4,5 +4,5 @@ module.exports = function(req, res, next) {
   res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
   res.setHeader("Pragma", "no-cache");
   res.setHeader("Expires", "0");
-  next();
+  next(); 
 }
